cc8e6d85fde79fb6fa8a435a6c5f46f2d5c5af6e
asolacion98 <f.acuna.v98@gmail.com>
2022-02-10 12:55:46 -0300

    modifique para que existiera un carusel de imagenes, tambien cambie de ubicacion el css de estilo

--- pro/Z/index.php
+++ pro/Z/index.php
@@ -10,12 +10,46 @@
     <!--CSS         materialize-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
     <!--CSS                    -->
-    <link rel="stylesheet" type="text/css" href="/css/estilos.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="./css/estilo.css" media="screen" />
+    <!--JQuery                 -->
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
     <title>PROYECTO Z</title>
 </head>
 
 <body>
     <?php require 'vista/navbar.php';?>
+
+    <div class="container">
+        <div class="section">
+            <div class="row">
+                <div class="col s12 m12">
+                    
+                    <div class="carousel carousel-slider">
+                        <a class="carousel-item" href="#"><img src="img/index/ash_blossom.jpg"></a>
+                        <a class="carousel-item" href="#"><img src="img/index/logo_yugioh.png"></a>
+                        </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+      $(document).ready(function(){
+        $('.carousel').carousel();
+      }, setTimeout(autoplay, 4500));
+
+
+     function autoplay() {
+       $('.carousel').carousel('next');
+       setTimeout(autoplay, 4500);
+     }
+
+    </script>
+
+
 
   <!--JavaScript  materialize-->
   <script type="text/javascript" src="js/materialize.min.js"></script>
